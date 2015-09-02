addf51a2647f1671481885d8ebafc03d935b57a6
Ryan Asensio <ryan.asensio@gmail.com>
2015-09-02 11:16:41 -0600

    got AR db to create Clients. good work russell

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,10 +1,5 @@
 module TrafficSpy
   class Client < ActiveRecord::Base
-
-    def initialize(params)
-      @identifier = params[:identifier]
-      @root_url = params[:rootUrl]
-    end
   end
 
 end
