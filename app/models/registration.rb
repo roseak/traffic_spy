f858f74ff358ae02ed120b25af2d821d1f8bb0a6
Ryan Asensio <ryan.asensio@gmail.com>
2015-09-01 18:45:10 -0600

    started creatind db

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,8 +1,10 @@
-class Registration # < ActiveRecord::Base
+module TrafficSpy
+  class Registration # < ActiveRecord::Base
 
-  def initialize(params)
-    @identifier = params[:identifier]
-    @root_url = params[:rootUrl]
+    def initialize(params)
+      @identifier = params[:identifier]
+      @root_url = params[:rootUrl]
+    end
   end
 
 end
