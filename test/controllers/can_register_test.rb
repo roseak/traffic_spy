4bdbee188b0307907f9218fbdb6791e248395ccf
russelleh <russell.west.eh@gmail.com>
2015-09-03 14:15:29 -0600

    It gets 400 from bad input params

--- test/controllers/can_register_test.rb
+++ test/controllers/can_register_test.rb
@@ -15,17 +15,7 @@ class RegisterTest < Minitest::Test
     assert_equal '{"identifier":"jumpstartlab"}', last_response.body
   end
 
-  def test_can_register_with_other_params
-    skip
-    attributes = {:identifier => 'jumpstartfab', :root_url=>'http://jumpstartfab.com'}
-    post('/sources', attributes)
-
-    assert_equal 200, last_response.status
-    assert_equal '{"identifier":"jumpstartfab"}', last_response.body
-  end
-
   def test_gets_400_from_bad_input_params
-    skip
     attributes = {:identifier => 'jumpstartlab'}
     post('/sources', attributes)
 
