b903f2452c9d99da21d90b5bddb0060de3f3a6b4
russelleh <russell.west.eh@gmail.com>
2015-09-03 14:27:31 -0600

    It gets 403 for duplicate payload

--- test/controllers/can_process_requests_test.rb
+++ test/controllers/can_process_requests_test.rb
@@ -64,7 +64,6 @@ class RegisterTest < Minitest::Test
   end
 
   def test_gets_403_for_duplicate_payload
-    skip
     attributes = {"identifier" => 'r3m', "rootUrl" => 'http://r3m.com'}
     post('/sources', attributes)
 
