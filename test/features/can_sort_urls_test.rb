2f4722c48800c3fcc2d561a5a9958052c0fc0ad7
Rose Kohn <rose.kohn@gmail.com>
2015-09-03 18:27:31 -0600

    Feature test for URLs

--- test/features/can_sort_urls_test.rb
+++ test/features/can_sort_urls_test.rb
@@ -2,17 +2,21 @@ require './test/test_helper'
 
 class UrlCanSortTest < FeatureTest
   include Rack::Test::Methods
-  attr_reader :payload1, :payload2, :payload3, :payload4, :payload5, :payload6
 
   def app
     TrafficSpy::Server
   end
 
   def setup
-    attributes = {:identifier => 'r3m', :root_url => 'http://r3m.com'}
+    DatabaseCleaner.start
+
+    attributes = {'identifier' => 'r3m', 'rootUrl' => 'http://r3m.com'}
     post('/sources', attributes)
 
-    @payload1 = JSON.parse('{
+    attributes2 = {'identifier' => '123', 'rootUrl' => 'http://123.com'}
+    post('/sources', attributes2)
+
+    payload1 = '{
       "url":"http://r3m.com/blog",
       "requestedAt":"2013-02-16 21:38:28 -0700",
       "respondedIn":37,
@@ -24,9 +28,9 @@ class UrlCanSortTest < FeatureTest
       "resolutionWidth":"1920",
       "resolutionHeight":"1280",
       "ip":"63.29.38.211"
-    }')
+    }'
 
-    @payload2 = JSON.parse('{
+    payload2 = '{
       "url":"http://r3m.com/blog",
       "requestedAt":"2013-01-16 21:38:28 -0700",
       "respondedIn":37,
@@ -38,9 +42,9 @@ class UrlCanSortTest < FeatureTest
       "resolutionWidth":"1920",
       "resolutionHeight":"1280",
       "ip":"63.29.38.211"
-    }')
+    }'
 
-    @payload3 = JSON.parse('{
+    payload3 = '{
       "url":"http://r3m.com/blog",
       "requestedAt":"2013-04-16 21:38:28 -0700",
       "respondedIn":37,
@@ -52,9 +56,9 @@ class UrlCanSortTest < FeatureTest
       "resolutionWidth":"1920",
       "resolutionHeight":"1280",
       "ip":"63.29.38.211"
-    }')
+    }'
 
-    @payload4 = JSON.parse('{
+    payload4 = '{
       "url":"http://r3m.com/pizza",
       "requestedAt":"2013-02-16 21:38:28 -0700",
       "respondedIn":37,
@@ -66,9 +70,9 @@ class UrlCanSortTest < FeatureTest
       "resolutionWidth":"1920",
       "resolutionHeight":"1280",
       "ip":"63.29.38.211"
-    }')
+    }'
 
-    @payload5 = JSON.parse('{
+    payload5 = '{
       "url":"http://r3m.com/jonothy",
       "requestedAt":"2013-02-16 21:38:28 -0700",
       "respondedIn":37,
@@ -80,9 +84,9 @@ class UrlCanSortTest < FeatureTest
       "resolutionWidth":"1920",
       "resolutionHeight":"1280",
       "ip":"63.29.38.211"
-    }')
+    }'
 
-    @payload6 = JSON.parse('{
+    payload6 = '{
       "url":"http://r3m.com/pizza",
       "requestedAt":"2013-07-16 21:38:28 -0700",
       "respondedIn":37,
@@ -94,24 +98,32 @@ class UrlCanSortTest < FeatureTest
       "resolutionWidth":"1920",
       "resolutionHeight":"1280",
       "ip":"63.29.38.211"
-    }')
-  end
+    }'
 
-  def app
-    TrafficSpy::Server
+    payload7 = '{
+      "url":"http://123.com/pizza",
+      "requestedAt":"2013-07-16 21:38:28 -0700",
+      "respondedIn":37,
+      "referredBy":"http://123.com",
+      "requestType":"GET",
+      "parameters":[],
+      "eventName": "socialLogin",
+      "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+      "resolutionWidth":"1920",
+      "resolutionHeight":"1280",
+      "ip":"63.29.38.211"
+    }'
+
+    post('/sources/r3m/data', {'payload' => payload1})
+    post('/sources/r3m/data', {'payload' => payload2})
+    post('/sources/r3m/data', {'payload' => payload3})
+    post('/sources/r3m/data', {'payload' => payload4})
+    post('/sources/r3m/data', {'payload' => payload5})
+    post('/sources/r3m/data', {'payload' => payload6})
+    post('/sources/123/data', {'payload' => payload7})
   end
 
   def test_can_get_list_of_urls_from_most_to_least_requested
-    skip
-    post('/sources/r3m/data', payload1)
-    post('/sources/r3m/data', payload2)
-    post('/sources/r3m/data', payload3)
-    post('/sources/r3m/data', payload4)
-    post('/sources/r3m/data', payload5)
-    post('/sources/r3m/data', payload6)
-
-
-    # sorted_urls = TrafficSpy::Url.sorted_ascending(identifier)
     visit('/sources/r3m')
     click_link('URLs')
     assert_equal "/sources/r3m/urls", current_path
@@ -122,10 +134,6 @@ class UrlCanSortTest < FeatureTest
     assert page.has_content?("2")
     assert page.has_content?("http://r3m.com/jonothy")
     assert page.has_content?("1")
-  end
-
-  def setup
-    DatabaseCleaner.start
   end
 
   def teardown
