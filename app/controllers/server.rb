65ba7dc414eb31ff60a5b3d5ac8c754aa89620a8
russelleh <russell.west.eh@gmail.com>
2015-09-03 19:24:13 -0600

    It can display urls

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -39,10 +39,8 @@ module TrafficSpy
         title: "URLs",
         header: "URL",
         comparison: "Requests",
-        data: TrafficSpy::Url.ranked_url_visits(identifier),
+        data: TrafficSpy::Url.ranked_url_visits(identifier).map { |k, v| [k.url, v] }.to_h,
       }
-
-      binding.pry
 
       erb :urls
     end
