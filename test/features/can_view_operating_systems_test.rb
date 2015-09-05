f19104eca78439d186e697f46d71450541678667
Rose Kohn <rose.kohn@gmail.com>
2015-09-05 15:47:20 -0600

    Add Web Browsers feature test, add get to the server, and edit operating systems test.

--- test/features/can_view_operating_systems_test.rb
+++ test/features/can_view_operating_systems_test.rb
@@ -1,12 +1,12 @@
 require './test/test_helper'
 
-class CanViewOperatingSystems < FeatureTest
+class CanViewOperatingSystemsTest < FeatureTest
   def test_can_see_list_of_operating_systems
     visit('/sources/r3m')
     click_link('Operating Systems')
     assert_equal '/sources/r3m/os', current_path
 
-    assert page.has_content?("Operating Systems")
+    assert page.has_content?("OS")
     assert page.has_content?("OS X 10.8.2")
     assert page.has_content?("3")
     assert page.has_content?("Windows 7")
