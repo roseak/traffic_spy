6f8b2b06306c9871d5a9afe8ea696eea59f7ac47
Ryan Asensio <ryan.asensio@gmail.com>
2015-09-06 14:31:33 -0600

    try to fix merge

--- app/models/client.rb
+++ app/models/client.rb
@@ -9,7 +9,8 @@ module TrafficSpy
 
     def self.prep(params)
       { :identifier => params.fetch("identifier", nil), 
-        :root_url => params.fetch("rootUrl", nil)}
+        :root_url => params.fetch("rootUrl", nil)
+      }
     end
 
     def self.avg_response_time(identifier)
